8f6a21a4eb9bd07970d894f0c1e6d22997c2f046
Ignacho <ignachobarberis@gmail.com>
2018-03-17 11:59:28 -0300

    ok

--- APP/app/Console/Commands/UpdateAsistencia.php
+++ APP/app/Console/Commands/UpdateAsistencia.php
@@ -40,8 +40,10 @@ class UpdateAsistencia extends Command
     {
         $this->info('Inicio Actualización Asistencias NO Confirmadas...');
 		//Actualizo las Asistencias no Confimadas al cierre del día.
-		$ConfirmAsis = AsistenciaCurso::where('estado_curso', 'G')
-					 ->update('estado_curso', 'C');
+		/*$ConfirmAsis = AsistenciaCurso::where('estado_curso', 'G')
+					 ->update('estado_curso', 'C');*/
+		$asis_curso = AsistenciaCurso::where('estado_curso', 'G')
+					->update(['estado_curso' => 'C']);		 
 		$this->info('Fin Actualización Asistencias NO Confirmadas...');					
     }
 }
